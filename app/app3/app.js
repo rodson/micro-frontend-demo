aa1342d027ff3d7e8caa91cd606e0109492a3ca9
rodsonluo <rodsonluo@tencent.com>
2024-03-15 14:40:57 +0800

    update iframe

--- app/app3/app.js
+++ app/app3/app.js
@@ -1,10 +1,18 @@
 const Koa = require('koa');
 const fs = require("fs");
 const app = new Koa();
+const Router = require('@koa/router');
+const router = new Router();
 
-app.use(async ctx => {
+router.get('/', async ctx => {
     ctx.set("Content-Type", "text/html");
     ctx.body = fs.readFileSync(__dirname + "/index.html");
 });
+
+router.get('/test', async ctx => {
+	ctx.body = 'fjsaldjflsadjflsj';
+});
+
+app.use(router.routes())
 
 app.listen(3000);
